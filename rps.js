89b5d8f743aca7f9d9edb85efae9e6d9382c0c7d
David Ravanbakhsh <ravanbak@gmail.com>
2021-11-19 16:47:19 -0800

    Create html and js files and add a few JavaScript game functions

---
+++ rps.js
@@ -0,0 +1,31 @@
+const plays = ['Rock','Paper','Scissors'];
+
+let computerPlay = () => plays[Math.round(Math.random() * 2)];
+
+function playRound(playerPlay, computerPlay) {
+    p = playerPlay.slice(0,1).toUpperCase();
+    c = computerPlay.slice(0,1).toUpperCase();
+
+    if (p === c) return 'Tie!';
+
+    let winner;
+
+    switch (p) {
+        case 'R':
+            winner = c === 'P' ? c : p;
+            break;
+        case 'P':
+            winner = c === 'S' ? c : p;
+            break;
+        case 'S':
+            winner = c === 'R' ? c : p;
+            break;
+    }
+
+    if (winner === p) {
+        return `You win! ${playerPlay} beats ${computerPlay}.`;
+    }
+    else {
+        return `You lose! ${computerPlay} beats ${playerPlay}.`;
+    }
+}
